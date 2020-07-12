cf2195aee8091d2fd032a719035328e3baea4ada
Felipe Urbanski <felipeurbansk@gmail.com>
2020-07-12 07:36:06 -0300

    Ended query builder and paginate

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LaraDev\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AddressController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
